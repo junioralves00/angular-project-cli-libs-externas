d6780eed7d2cd4114fc11809e1579bef678667f0
junioralves00 <junioralves00@gmail.com>
2020-04-27 09:51:56 -0300

    Project Full

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+//import { MaterializeModule } from "angular2-materialize";
 @NgModule({
   declarations: [
     AppComponent
